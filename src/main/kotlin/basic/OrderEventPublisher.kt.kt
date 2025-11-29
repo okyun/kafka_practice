d4e36ec3ab8b32931992b62f566bcc976e73048a
okyun <dhrdbs8066@naver.com>
2025-11-29 21:44:38 +0900

    클래스명 수정

--- src/main/kotlin/basic/OrderEventPublisher.kt.kt
+++ src/main/kotlin/basic/OrderEventPublisher.kt.kt
@@ -10,12 +10,12 @@ import kotlin.math.log
 
 //이벤트 전송 Publisher
 @Component
-class `OrderEventPublisher.kt`(
+class OrderEventPublisher(
     private val kafkaTemplate: KafkaTemplate<String, OrderEvent>,//Kafka로 메시지를 보내는 기능을 제공하는 "전용 템플릿"
     //topic값도 환경변수로 잡기
     @Value("\${kafka.topics.orders}") private val ordersTopic: String
 ) {
-    private val logger = LoggerFactory.getLogger(`OrderEventPublisher.kt`::class.java)
+    private val logger = LoggerFactory.getLogger(OrderEventPublisher::class.java)
     //파티션의 저장공간을 정하기 위한 key(orderEvent.orderId)
     //같은 키는 같은 파티션에 저장
     fun publishOrderEvent(orderEvent: OrderEvent) {
